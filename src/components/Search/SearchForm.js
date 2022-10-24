43680d7be3536906d185b082b5bd6b042e3c3f92
kabinny <kabinny@gmail.com>
2022-10-24 17:57:19 +0900

    Feat : 리셋 버튼 추가

--- src/components/Search/SearchForm.js
+++ src/components/Search/SearchForm.js
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react"
 import { useRecoilState } from "recoil"
+import { faXmark } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import SearchModal from "./SearchModal"
 import { selectedRegion01, selectedRegion02 } from "../../recoil/regionState"
@@ -9,6 +11,14 @@ function SearchForm() {
 
   const [depth01, setDepth01] = useRecoilState(selectedRegion01)
   const [depth02, setDepth02] = useRecoilState(selectedRegion02)
+
+  // 지역 선택 리셋
+  function clickReset(event) {
+    event.preventDefault()
+    setDepth01("")
+    setDepth02("")
+    setSearchModalOpen(false)
+  }
 
   return (
     <div className="ml-4">
@@ -26,6 +36,15 @@ function SearchForm() {
             disabled
           />
         </span>
+
+        {/* 검색 클리어 */}
+        <button
+          type="reset"
+          className={`ml-1 align-middle ${depth01 === "" ? "hidden" : ""}`}
+          onClick={clickReset}
+        >
+          <FontAwesomeIcon icon={faXmark} className="text-2xl text-rose-300" />
+        </button>
 
         {/* 지역 선택 모달 */}
         <SearchModal
