d7e8a2fa72af99dc418566969ab2e768bbb19dd7
devYuMinKim <gimyumin40@gmail.com>
2022-10-26 19:00:17 +0900

    Style : 로그인 페이지 디자인 변경

--- src/components/Login/KaKaoLoginButton.js
+++ src/components/Login/KaKaoLoginButton.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React from "react"
+
+import kakao_login_button from "../../assets/login/kakao_login_button.png"
 
 // 카카오 개발자 앱 키 선언
-const REDIRECT_URI = "http://localhost:3000/oauth2/redirect"; // redirect 주소
-const auth = `http://Teamrocket-1780545001.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URI}`;
+const REDIRECT_URI = "http://localhost:3000/oauth2/redirect" // redirect 주소
+const auth = `http://Teamrocket-1780545001.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URI}`
 // http://localhost:8080
 const KakaoLoginButton = () => {
   return (
+    // <a href={auth} rel="noopener noreferrer">
+    //   <img
+    //     src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+    //     width="200"
+    //   />
+    // </a>
     <a href={auth} rel="noopener noreferrer">
-      <button>카카오로 시작하기</button>
+      <img src={kakao_login_button} width="200" alt="kakao" />
     </a>
-  );
-};
+  )
+}
 
-export default KakaoLoginButton;
+export default KakaoLoginButton
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,28 @@
+import KakaoLoginButton from "../components/Login/KaKaoLoginButton"
+import NaverLoginButton from "../components/Login/NaverLoginButton"
+import GoogleLoginButton from "../components/Login/GoogleLoginButton"
+
 function Login() {
-  return <>Login</>
+  return (
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-pink-700">
+          로그인
+        </h1>
+        <form className="login-form grid place-items-center my-4">
+          <div className="kakao my-1">
+            <KakaoLoginButton />
+          </div>
+          <div className="naver my-1">
+            <NaverLoginButton />
+          </div>
+          <div className="google">
+            <GoogleLoginButton />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Login
