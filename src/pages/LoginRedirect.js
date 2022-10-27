801cbf0c5cd11ff95bb59e9b3441fcd7ec143803
kabinny <kabinny@gmail.com>
2022-10-27 21:23:27 +0900

    Feat : 로그인 성공 실패 보여주기

--- src/pages/LoginRedirect.js
+++ src/pages/LoginRedirect.js
@@ -1,9 +1,14 @@
+import { useEffect, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import Container from "../components/Layout/Container"
+
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function LoginRedirect() {
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
+  const [message, setMessage] = useState("")
 
   console.log(searchParams.get("error"))
 
@@ -13,13 +18,24 @@ function LoginRedirect() {
   // token 저장
   localStorage.setItem("token", token)
 
-  setTimeout(() => {
-    navigate("/", { replace: true })
-  }, 3000)
+  useEffect(() => {
+    // 메세지
+    setMessage(`${token === null ? "실패" : "성공"}`)
+    // 페이지 이동
+    setTimeout(() => {
+      navigate("/", { replace: true })
+    }, 2000)
+  }, [])
 
   return (
     <Container>
-      <h2>로그인 완료? 페이지 이동중</h2>
+      <h2 className="text-rose-400 font-bold text-2xl text-center pt-2">
+        로그인 {message}! 페이지 이동중
+        <FontAwesomeIcon
+          icon={faSpinner}
+          className="ml-1 text-2xl text-rose-500 fa-spin"
+        />
+      </h2>
     </Container>
   )
 }
