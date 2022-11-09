1a67b952a00acb33c9e6061054162d5bb0a56311
kabinny <kabinny@gmail.com>
2022-11-09 21:26:48 +0900

    Fix : 채팅방에서는 헤더 안보이게 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import HeaderMenu from "./HeaderMenu"
 import Logo from "./Logo"
 import SearchForm from "../Search/SearchForm"
+import { useLocation } from "react-router-dom"
 
 function Header() {
   const [modalOpen, setModalOpen] = useState(false)
+
+  const location = useLocation()
+  if (location.pathname.indexOf("/chatroom") >= 0) return null
 
   return (
     <header className="border-b z-20 relative">
