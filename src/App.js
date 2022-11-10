17132420b85d415bc0cf4f9cacef7ecea13a5908
kabinny <kabinny@gmail.com>
2022-11-10 17:57:34 +0900

    Fix : 없는 페이지 임시 주석 처리

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import ChatRoom from "./pages/ChatRoom"
 import LoginRedirect from "./pages/LoginRedirect"
 import CreateFeed from "./pages/CreateFeed"
 
-import MyFeedListPage from "./pages/MyFeedListPage"
-import DetailedFeed from "./pages/DetailedFeed"
+// import MyFeedListPage from "./pages/MyFeedListPage"
+// import DetailedFeed from "./pages/DetailedFeed"
 import EditFeed from "./pages/EditFeed"
 
 function App() {
@@ -27,8 +27,8 @@ function App() {
         <Route path="/createchat" element={<CreateChatPage />} />
         <Route path="/chatroom/:roomId" element={<ChatRoom />} />
         <Route path="/createfeed" element={<CreateFeed />} />
-        <Route path="/myfeedlist" element={<MyFeedListPage />} />
-        <Route path="/detailedfeed/:feedId" element={<DetailedFeed />} />
+        {/* <Route path="/myfeedlist" element={<MyFeedListPage />} />
+        <Route path="/detailedfeed/:feedId" element={<DetailedFeed />} /> */}
         <Route path="/editfeed/:feedId" element={<EditFeed />} />
 
         {/* login redirect */}
