9dc8dd367de60cf8036b6f05cbdfeeeed883f8f6
kabinny <kabinny@gmail.com>
2022-11-17 18:04:26 +0900

    chat api 주소 수정

--- src/apis.js
+++ src/apis.js
@@ -1,4 +1,5 @@
 export const DEFAULT_API =
   "http://Teamrocket-1780545001.ap-northeast-2.elb.amazonaws.com"
 
-export const CHAT_API = "http://52.78.188.101:8080"
+// export const CHAT_API = "http://52.78.188.101:8080"
+export const CHAT_API = "http://3.36.97.155:8080"
